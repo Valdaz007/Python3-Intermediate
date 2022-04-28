d48bfb31fee6d3d082b35fc75da947bbe5b452d5
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2022-04-29 08:50:04 +1000

    Connecting Python Apps to a Server
    These are the Basic Scripts for the Client - Server Models. The Scripts for both Client and Server side that will handle data to from both the both the client app and server.

---
+++ Connecting App to Server/client.py
@@ -0,0 +1,11 @@
+import requests
+
+def main():
+	# Send a 'GET' Request to the Server
+	data_set = requests.get(f"http://beta.valdazmedia.com/betatest")
+	
+	# Print Data to the Console
+	print(data_set.json())
+
+if __name__ == "__main__":
+    main()
---
+++ Connecting App to Server/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+import json, time
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+	data_set = {'Page': 'Home', 'Msg': 'Hello, World!', 'Timestamp': time.time()}
+	json_dump = json.dump(data_set)
+	return json.dump
