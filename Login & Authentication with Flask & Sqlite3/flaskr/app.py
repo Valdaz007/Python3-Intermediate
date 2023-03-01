e091be5aa21ef840c4a243079fc54fb0c782ce11
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2023-03-02 07:05:07 +1000

    Update app.py

--- Login & Authentication with Flask & Sqlite3/flaskr/app.py
+++ Login & Authentication with Flask & Sqlite3/flaskr/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return render_template('index.html', title='Home')
+    return render_template('index.html', title = "Home")
+
+@app.route("/login")
+def login():
+    return render_template('login.html', title = "Login")
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0")
+    app.run(debug=True)
