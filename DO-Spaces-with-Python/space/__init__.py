3d4cec0c815c4890d5152538c1e572112b4cf0e4
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2023-03-22 06:40:22 +1000

    Create __init__.py

---
+++ DO-Spaces-with-Python/space/__init__.py
@@ -0,0 +1,31 @@
+import boto3
+
+class Space:
+    def __init__(self, region, endpoint, access_id, access_secret, space_name):
+        session = boto3.session.Session()
+        self.client = session.client(
+            's3',
+            region_name = region,
+            endpoint_url = endpoint,
+            aws_access_key_id = access_id,
+            aws_secret_access_key = access_secret
+        )
+        self.space_name = space_name
+    
+    def get_Space_Name(self):
+        return self.space_name
+    
+    def get_Space_File_List(self):
+        response = self.client.list_objects(Bucket = self.space_name)
+        return response
+    
+    def download_File(self, filename):
+        try:
+            self.client.download_file(
+            self.space_name,
+            filename,
+            f"./{filename}"
+            )
+            return True
+        except:
+            return False
