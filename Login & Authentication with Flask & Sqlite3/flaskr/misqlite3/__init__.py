3a114ef294c3c0613b47bdebcacb702a81c71f8a
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2023-03-28 10:37:42 +1000

    Update __init__.py
    Update Constructor and getUser() function

--- Login & Authentication with Flask & Sqlite3/flaskr/misqlite3/__init__.py
+++ Login & Authentication with Flask & Sqlite3/flaskr/misqlite3/__init__.py
@@ -1,23 +1,27 @@
 import sqlite3
 
 class DB:
-    def __init__(self):
+    def __init__(self, dbFile):
+        self.dbFile = dbFile
         self.conxn = None
         self.cursor = None
 
     def open(self, dbFile):
-        self.conxn = sqlite3.connect(dbFile)
+        self.conxn = sqlite3.connect(self.dbFile)
         self.cursor = self.conxn.cursor()
 
     def close(self):
         self.conxn.close()
 
 class Login(DB):
-    def getUser(self, uname):
-        self.cursor.execute(f"SELECT * FROM users WHERE uname = '{uname}'")
+    def getUser(self, uname, upwd):
+        self.open()
+        self.cursor.execute(f"SELECT * FROM users WHERE uname = '{uname}' AND upwd = '{upwd}'")
         self.conxn.commit()
-        results = self.cursor.fetchall()[0]
-        return results
+        results = self.cursor.fetchall()
+        if len(results) >= 1:
+            return True if results[0][3] == uname and results[0][4] == upwd else False
+        else: return False
 
 class SignUp(DB):
     pass
