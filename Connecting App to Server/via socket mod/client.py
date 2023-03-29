1059dd3455ca234e2bd9ea21236f40b9c05704d0
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2023-03-29 14:29:37 +1000

    Update client.py
    Added to recv_msg() function to recieve msgs from server side.

--- Connecting App to Server/via socket mod/client.py
+++ Connecting App to Server/via socket mod/client.py
@@ -7,12 +7,21 @@ def send(msg, client_socket):
     msg_len_Bytes += b' ' * (64-len(msg_len_Bytes))
     client_socket.send(msg_len_Bytes)
     client_socket.send(msg_Bytes)
+    
+def recv_msg(conxn):
+    msg_len = conxn.recv(64).decode('utf-8')
+    if msg_len:
+        msg_len = int(msg_len)
+        msg = conxn.recv(msg_len).decode('utf-8')
+        return msg
 
 def chatapp():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('server_ip_addr', 5050))
     send("Hello, World!", client_socket)
+    print(f"[FROM SERVER] {recv_msg(client_socket)}")
     send("!DISCONNECTED", client_socket)
+    print(f"[FROM SERVER] {recv_msg(client_socket)}")
     
 if __name__ == "__main__":
     chatapp()
