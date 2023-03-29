e3b031921dd5c14dd428dc64cf3db1b0c2e3e050
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2023-03-29 14:27:23 +1000

    Update server.py
    Added send() and recv_msg() function.
    These function will send and receive messages from client side.

--- Connecting App to Server/via socket mod/server.py
+++ Connecting App to Server/via socket mod/server.py
@@ -4,24 +4,37 @@ import threading
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((socket.gethostbyname(socket.gethostname()), 5050))
 
+def send(msg, conxn):
+    msg_Bytes = msg.encode('utf-8')
+    msg_len_Decimal = len(msg)
+    msg_len_Bytes = str(msg_len_Decimal).encode('utf-8')
+    msg_len_Bytes += b' ' * (64-len(msg_len_Bytes))
+    conxn.send(msg_len_Bytes)
+    conxn.send(msg_Bytes)
+    
+def recv_msg(conxn):
+    msg_len = conxn.recv(64).decode('utf-8')            # Blocker: Wait For Msg From Client
+    if msg_len:
+        msg_len = int(msg_len)
+        msg = conxn.recv(msg_len).decode('utf-8')
+        return msg
+
 def client_connection_thread(conn, addr):
     print(f"[NEW CONNECTION] {addr} Connected!")
     connected = True
     while connected:
-        msg_length = conn.recv(64).decode('utf-8')
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode('utf-8')
-            if msg == "!DISCONNECTED":
-                connected = False
-            print(f"[{addr}] {msg}")
+        msg = recv_msg(conn)
+        if msg == "!DISCONNECTED":
+            connected = False
+        print(f"[{addr}] {msg}")
+        send("Copy that!", conn)
     conn.close()
 
 def start():
     server_socket.listen()
     while True:
-        conn, addr = server_socket.accept()
-        thread = threading.Thread(target=client_connection_thread, args=(conn, addr))
+        conn, addr = server_socket.accept()             # Blocker: Wait For Connections From Client
+        thread = threading.Thread(target=client_connection_thread, args=(conn, addr))   # Establish Connection with New Thread to Handler Individual Client
         thread.start()
         print(f"[ACTIVE CONNECTION] {threading.activeCount()-1}")
 
